41efecc2051f936225248701c7cf171fec6f214d
pcorrales <pcorrales@estudiantec.cr>
2018-10-28 12:17:14 -0600

    IConstructorCuenta lista

--- src/logicaDeNegocios/IConstructorCuenta.java
+++ src/logicaDeNegocios/IConstructorCuenta.java
@@ -4,6 +4,16 @@ import dto.DTOCuenta;
 
 public interface IConstructorCuenta {
 	public abstract Cuenta construirRegistro(DTOCuenta pNumeroCuenta, DTOCuenta pPin);
+	public abstract Cuenta construirDeposito(int pNumeroCuenta, double pMonto);
+	public abstract Cuenta construirDepositoCambioMoneda(int pNumeroCuenta, int pMonto);
+	public abstract Cuenta construirRetiroColones(int pNumeroCuenta,String pPin, int pMonto, int pCuentaDestino);
+	public abstract Cuenta construirTransferencia(int pNumeroCuentaOrigen, String pPin, int pMonto, int pCuentaDestino);
+	public abstract Cuenta construirConsultaSaldo(int pNumeroCuenta, String pPin);
+	public abstract Cuenta construirConsultaSaldoMonedaExtranjera(int pNumeroCuenta, String pPin);
+	public abstract Cuenta construirConsultaEstadoCuenta(int pNumeroCuenta, String pPin);
+	public abstract Cuenta construirConsultaEstadoCUentaMonedaExtranjera(int pNumeroCuenta, String pPin);
+	public abstract Cuenta construirCambioTelefono(int pNumeroCuenta, String pPin, String pTelefono);
+	public abstract Cuenta construirCambioCorreo(int pNumeroCuenta, String pPin, String pCorreo);
 
 	public abstract Cuenta construirConsultaDatosCuenta(DTOCuenta pDatosCuenta);
 	

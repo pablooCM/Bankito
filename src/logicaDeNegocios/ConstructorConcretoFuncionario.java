b354f6a06c13f26e1306de19c6b91a0ee10313d1
pcorrales <pcorrales@estudiantec.cr>
2018-10-27 21:31:48 -0600

    Cositas que se hacen y así

--- src/logicaDeNegocios/ConstructorConcretoFuncionario.java
+++ src/logicaDeNegocios/ConstructorConcretoFuncionario.java
@@ -1,5 +1,19 @@
 package logicaDeNegocios;
 
-public class ConstructorConcretoFuncionario {
+import dto.DTOCuenta;
+
+public class ConstructorConcretoFuncionario implements IConstructorCuenta{
+
+	@Override
+	public Cuenta construirRegistro(DTOCuenta pNumeroCuenta, DTOCuenta pPin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cuenta construirConsultaDatosCuenta(DTOCuenta pDatosCuenta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/logicaDeNegocios/IConstructorCuenta.java
+++ src/logicaDeNegocios/IConstructorCuenta.java
@@ -1,5 +1,10 @@
 package logicaDeNegocios;
 
-public interface IConstructorCuenta {
+import dto.DTOCuenta;
 
+public interface IConstructorCuenta {
+	public abstract Cuenta construirRegistro(DTOCuenta pNumeroCuenta, DTOCuenta pPin);
+
+	public abstract Cuenta construirConsultaDatosCuenta(DTOCuenta pDatosCuenta);
+	
 }
